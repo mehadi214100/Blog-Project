5504989fb1abf49c877bbbcaff641736e8a862dc
mehadi214100 <mehadi214100@gmail.com>
2025-03-21 22:33:56 +0600

    blog model added

--- blog/admin.py
+++ blog/admin.py
@@ -6,5 +6,12 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id','category_name','created_at','updated_at')
 
 
+class blogAdmin(admin.ModelAdmin):
+    list_display = ('id','title','author','category','blog_image','status','is_featured')
+    prepopulated_fields ={
+        'slug':('title',)
+    }
+
 
 admin.site.register(models.Category,CategoryAdmin)
+admin.site.register(models.Blog,blogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Category(models.Model):
     category_name = models.CharField(max_length=100,unique=True)
     created_at = models.DateField(auto_now_add=True)
@@ -8,3 +8,23 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+    def __str__(self):
+        return self.category_name
+
+STATUS_CHOICE = (
+    ('draft','Draft'),
+    ('publish','Published')
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250,unique=True)
+    slug = models.SlugField(unique=True,blank=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    blog_image = models.ImageField(upload_to='uploads/%y/%m/%d')
+    short_desc = models.TextField(max_length=1000)
+    blog_body = models.TextField()
+    status = models.CharField(max_length=100,choices=STATUS_CHOICE,default='draft')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
