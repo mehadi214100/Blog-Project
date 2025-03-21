3ef1385abb53b48bf1f4180d10bd48b01a2c9f0d
mehadi214100 <mehadi214100@gmail.com>
2025-03-22 00:42:59 +0600

    template inherit and category link added

--- blog/models.py
+++ blog/models.py
@@ -28,3 +28,6 @@ class Blog(models.Model):
     is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from . import models
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def home(request):
@@ -12,3 +14,18 @@ def home(request):
         'allpost':allpost
     }
     return render(request,'index.html',contex)
+
+def posts_by_category(request,category_id):
+    posts = models.Blog.objects.filter(status='publish',category=category_id)
+    # try:
+    #     categorys = models.Category.objects.get(id=category_id)
+    # except:
+    #     return redirect('/')
+
+    categorys = get_object_or_404(models.Category,pk=category_id)
+
+    contex = {
+        'posts':posts,
+        'categorys':categorys
+    }
+    return render(request,'posts_by_category.html',contex)
